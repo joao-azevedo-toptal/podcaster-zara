4601f8635fd10ba92bdd502f8c0b7bfecf0301b4
João Azevedo <joao.azevedo@toptal.com>
2023-02-24 10:55:24 +0000

    Add client side routing and tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Home from "./views/Home";
+import Podcast from "./views/Podcast";
+import Episode from "./views/Episode";
 
+export default function App() {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/podcast/1">Podcast</Link>
+        <Link to="/podcast/1/episode/1">Episode</Link>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/podcast/:podcastId" element={<Podcast />}></Route>
+          <Route
+            exact
+            path="/podcast/:podcastId/episode/:episodeId"
+            element={<Episode />}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
-
-export default App
---
+++ src/views/Podcast.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Podcast() {
+  return <h1>Podcast</h1>;
+}
